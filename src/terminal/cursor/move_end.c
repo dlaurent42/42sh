5a74645ea707b8784ef9aae3b9a9b2701f3564c1
Damien LAURENT-NAUGUET <dlaurent@e1r9p6.42.fr>
2018-09-13 21:03:27 +0200

    bug: minor correction

--- src/terminal/cursor/move_end.c
+++ src/terminal/cursor/move_end.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 19:05:41 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/13 15:46:07 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/09/13 21:03:01 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,11 +21,14 @@ void		sh_move_end(t_shell *shell)
 		ft_putstr(K_DOWN);
 		shell->term->cursor.y++;
 	}
-	while (shell->term->cursor.x < (shell->read->buffer.display_length + shell->term->header.display_length_mod + 1) % shell->term->w_width)
+	while ((shell->term->cursor.y && shell->term->cursor.x < (shell->read->buffer.display_length + shell->term->header.display_length_mod + 1) % shell->term->w_width)
+	|| (shell->term->cursor.y == 0 && shell->term->cursor.x < shell->read->buffer.display_length + shell->term->header.display_length_mod + 1))
 	{
 		ft_putstr(K_RIGHT);
 		shell->term->cursor.x++;
 	}
+	if (shell->term->cursor.y == 0)
+		shell->term->cursor.x = shell->read->buffer.display_length;
 	shell->term->cursor.abs_pos = shell->read->buffer.display_length;
 	shell->term->cursor.rel_pos = shell->read->buffer.unicode_length;
 }
