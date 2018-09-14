160716612a686e2dfabd001d97dcb62f8bc9b7c7
dlaurentn <dlaurent@student.42.fr>
2018-09-14 19:56:37 +0200

    fix bugs

--- src/terminal/cursor/move_xy.c
+++ src/terminal/cursor/move_xy.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/03 19:05:41 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/14 16:50:21 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/09/14 19:56:29 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -42,17 +42,8 @@ static void	sh_move_x(t_shell *shell, unsigned int x)
 void		sh_move_to_xy(t_shell *shell, unsigned int x, unsigned int y)
 {
 	shell->term->cursor.y -= (shell->term->cursor.x == 0 && shell->term->cursor.y) ? 1 : 0;
-	sh_debug(NULL, ft_itoa(shell->term->cursor.x));
-	sh_debug(NULL, ft_itoa(shell->term->cursor.y));
-	sh_debug(NULL, ft_itoa(x));
-	sh_debug(NULL, ft_itoa(y));
-	sh_debug(shell, "avant home");
 	sh_move_home(shell);
-	sh_debug(shell, "avant move y");
 	sh_move_y(shell, y);
-	sh_debug(shell, "avant move x");
 	sh_move_x(shell, x);
-	sh_debug(shell, "avant set pos");
 	sh_set_rel_pos(shell, shell->term->cursor.abs_pos, 1);
-	sh_debug(shell, "apres set pos");
 }
