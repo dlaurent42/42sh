f25e14849b3b042826d4f39289a1f9de3f41c411
Damien LAURENT-NAUGUET <dlaurent@e2r3p15.42.fr>
2018-09-16 17:05:49 +0200

    [feature] prompt personalization

--- src/structures/term/set_prompt.folder.c
+++ src/structures/term/set_prompt.folder.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/16 15:07:52 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/16 15:53:08 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/09/16 17:01:50 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,12 @@
 char	*term_get_folder_name(t_env *env, char *location, size_t length)
 {
 	if (ft_strcmps((char *)location, env_search(env, "HOME")) == 0)
-		return (ft_strdup("~"));
+		return (ft_strdup("\e[1;32;40m~"));
 	if (length == 1 && location[0] == '/')
-		return (ft_strdup("/"));
+		return (ft_strdup("\e[1;32;40m/"));
 	(location[length - 1] == '/') ? location[length--] = '\0' : 0;
 	if ((location[0] == '/' && ft_strcountif((char *)location, '/') == 1))
-		return (ft_strdup(location));
+		return (ft_strjoins("\e[1;32;40m", location));
 	length--;
 	while (location[length] && location[length] != '/')
 		length--;
