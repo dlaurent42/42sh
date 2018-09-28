4c527802e64d07158749976f5a3a3963ec2cf1b1
Damien LAURENT-NAUGUET <dlaurent@e2r4p8.42.fr>
2018-09-29 01:37:51 +0200

    [bug] fix previous word deletion

--- src/terminal/delete/delete_left_word.c
+++ src/terminal/delete/delete_left_word.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 14:35:28 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/27 15:24:23 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/09/29 01:37:04 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,16 @@
 
 void	sh_delete_previous_word(t_shell *sh)
 {
+	int		del;
+
+	del = sh->cursor.abs_pos;
 	if (sh->cursor.abs_pos == 0)
 		return ;
-	while (sh->cursor.rel_pos
-	&& ((unsigned char)sh->buffer.content[sh->cursor.rel_pos] == ' '
-	|| (unsigned char)sh->buffer.content[sh->cursor.rel_pos] == '\t'))
-		sh_delete_char(sh);
-	while (sh->cursor.rel_pos
-	&& (unsigned char)sh->buffer.content[sh->cursor.rel_pos - 1] != ' '
-	&& (unsigned char)sh->buffer.content[sh->cursor.rel_pos - 1] != '\t')
-		sh_delete_char(sh);
+	sh_move_previous_word(sh);
+	del -= sh->cursor.abs_pos;
+	while (del >= 0)
+	{
+		sh_delete_current_char(sh);
+		del--;
+	}
 }
