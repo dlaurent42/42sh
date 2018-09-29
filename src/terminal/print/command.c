ae2e2972112ef530c052ae00a387e6dfa529e1bc
Damien LAURENT-NAUGUET <dlaurent@e2r2p23.42.fr>
2018-09-29 18:41:43 +0200

    [feature] simplication of cmd printing

--- src/terminal/print/command.c
+++ src/terminal/print/command.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/28 14:32:50 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/28 15:25:28 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/09/29 18:39:12 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,6 +16,16 @@ void	sh_print_cmd(t_shell *sh, t_cmd *cmd)
 {
 	sh->buffer.cmd = cmd;
 	sh_delete_all(sh);
-	sh_print_str(sh, cmd->content);
+	ft_strcpy(sh->buffer.content, cmd->content);
+	ft_putstr(sh->buffer.content);
+	sh->buffer.unicode_len = cmd->unicode_len;
+	sh->buffer.display_len = cmd->display_len;
+	sh->cursor.abs_pos = sh->buffer.display_len;
+	sh->cursor.rel_pos = sh->buffer.unicode_len;
+	sh->cursor.y = (sh->buffer.display_len + sh->prompt.len_mod)
+	/ sh->window.width;
+	sh->cursor.x = (sh->cursor.y)
+		? (sh->buffer.display_len + sh->prompt.len_mod) % sh->window.width
+		: sh->buffer.display_len;
 	sh->modes.browse = TRUE;
 }
