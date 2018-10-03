ba7bfc217025c7a33cc70ee11c4fcb7090a6fe32
Damien LAURENT-NAUGUET <dlaurent@e2r2p14.42.fr>
2018-10-03 20:54:18 +0200

    [feature] tilde handling

--- src/functions/cd/cd.c
+++ src/functions/cd/cd.c
@@ -6,32 +6,51 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 18:20:50 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/03 18:53:12 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/03 20:50:21 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+static char	sh_cd_tilde_handler(t_shell *sh, char *s, int option_p)
+{
+	char	res;
+	char	*home;
+	char	*path;
+
+	if (!(home = env_search(sh->env, "HOME")))
+		return (sh_cd_error(NULL, NULL, 5));
+	if (!(path = ft_strjoins(home, s + 1)))
+		return (sh_cd_error(NULL, NULL, 5));
+	res = (option_p)
+		? sh_cd_nofollow(sh, path, NULL, FALSE)
+		: sh_cd_follow(sh, path, FALSE);
+	ft_strdel(&path);
+	return (res);
+}
 
 static char	sh_cd_dispatch(t_shell *sh, int option_p, char **argv, int i)
 {
 	char	res;
 
 	res = 0;
-	if (ft_count_argv((void **)argv) > 1)
+	if (ft_count_argv((void **)argv + i) > 1)
 		res = sh_cd_error(NULL, NULL, 7);
 	else if (!argv[i] || (argv[i][0] == '~' && !argv[i][1])
 	|| (argv[i][0] == '-' && argv[i][1] == '-' && !argv[i][2]))
 		res = (env_search(sh->env, "HOME"))
 			? sh_cd_nofollow(sh, env_search(sh->env, "HOME"), NULL, FALSE)
 			: sh_cd_error(NULL, NULL, 5);
-	else if (argv[i] && argv[i + 1])
-		res = sh_cd_error(argv[i], NULL, 5);
+	else if (argv[i][0] == '~')
+		res = sh_cd_tilde_handler(sh, argv[i], option_p);
 	else if (argv[i][0] == '-' && !argv[i][1] && env_search(sh->env, "OLDPWD"))
 		res = (option_p)
 			? sh_cd_nofollow(sh, env_search(sh->env, "OLDPWD"), NULL, TRUE)
 			: sh_cd_follow(sh, env_search(sh->env, "OLDPWD"), TRUE);
 	else if (argv[i][0] == '-' && !argv[i][1])
-		res = sh_cd_error(argv[i], NULL, 6);
+		res = sh_cd_error(NULL, NULL, 6);
+	else if (argv[i] && argv[i + 1])
+		res = sh_cd_error(argv[i], NULL, 7);
 	else
 		res = (option_p)
 			? sh_cd_nofollow(sh, argv[i], NULL, FALSE)
