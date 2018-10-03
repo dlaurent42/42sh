e07f36a32348e2e85d1113b9e9b8371de22629d8
davhojt <davhojt@gmail.com>
2018-10-03 19:13:15 +0200

    [feat] Broke list generator for binary autocompletion out of insert.

---
+++ src/structures/binaries/gen_list_for_auto_comp.c
@@ -0,0 +1,31 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   gen_list_for_auto_comp.c                           :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/10/03 18:55:08 by dhojt             #+#    #+#             */
+/*   Updated: 2018/10/03 18:55:30 by dhojt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+void				bin_gen_list_for_auto_comp(t_shell *sh, t_bin *bin, char *s)
+{
+	t_bin_auto		*new;
+
+	if (ft_strcmps(s, ".") && ft_strcmps(s, ".."))
+	{
+		if (!(new = (t_bin_auto *)ft_memalloc(sizeof(t_bin_auto))))
+			error_malloc_bin(sh, bin, "t_bin_auto structure");
+		if (!(new->name = ft_strdup(s)))
+		{
+			free(new);
+			error_malloc_bin(sh, bin, "t_bin_auto name string");
+		}
+		new->next = bin->bin_auto;
+		bin->bin_auto = new;
+	}
+}
