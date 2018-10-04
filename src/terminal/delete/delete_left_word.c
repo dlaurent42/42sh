146d2014c0f5ae4b773313f6fd5ad1438cab1b28
Damien LAURENT-NAUGUET <dlaurent@e1r6p6.42.fr>
2018-10-04 15:12:12 +0200

    [feature] improve speed of deletion

--- src/terminal/delete/delete_left_word.c
+++ src/terminal/delete/delete_left_word.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 14:35:28 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/03 23:56:48 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/04 14:59:07 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -29,7 +29,24 @@ static void	sh_move_cursor(t_shell *sh)
 		: sh->buffer.display_len;
 	sh->cursor.abs_pos = sh->buffer.display_len;
 	sh->cursor.rel_pos = sh->buffer.unicode_len;
+	sh->buffer.display_len = ft_strlenu(sh->buffer.content);
+	sh->buffer.unicode_len = ft_strlens(sh->buffer.content);
 	sh_move_to_xy(sh, x, y);
+}
+
+static void	sh_delete_words(t_shell *sh, int i, int rel_pos_end)
+{
+	while (i < rel_pos_end)
+	{
+		sh->buffer.content[i] =
+		sh->buffer.content[rel_pos_end + i - sh->cursor.rel_pos];
+		i++;
+	}
+	while (sh->buffer.content[i])
+	{
+		sh->buffer.content[i] = '\0';
+		i++;
+	}
 }
 
 void		sh_delete_previous_word(t_shell *sh)
@@ -38,23 +55,19 @@ void		sh_delete_previous_word(t_shell *sh)
 	int		abs_pos_end;
 	int		rel_pos_end;
 
-	abs_pos_end = sh->cursor.abs_pos;
-	rel_pos_end = sh->cursor.rel_pos;
 	if (sh->cursor.abs_pos == 0)
 		return ;
+	while (sh->buffer.content[sh->cursor.rel_pos] == ' '
+	|| sh->buffer.content[sh->cursor.rel_pos] == '\t')
+		sh_move_right(sh);
+	abs_pos_end = sh->cursor.abs_pos;
+	rel_pos_end = sh->cursor.rel_pos;
 	sh_move_previous_word(sh);
 	i = sh->cursor.rel_pos;
 	sh->buffer.display_len = sh->buffer.display_len
 		- abs_pos_end + sh->cursor.abs_pos;
 	sh->buffer.unicode_len = sh->buffer.unicode_len
 		- rel_pos_end + sh->cursor.rel_pos;
-	while (i < rel_pos_end)
-	{
-		sh->buffer.content[i] =
-		sh->buffer.content[rel_pos_end + i - sh->cursor.rel_pos];
-		i++;
-	}
-	while (sh->buffer.content[i])
-		sh->buffer.content[i++] = '\0';
+	sh_delete_words(sh, i, rel_pos_end);
 	sh_move_cursor(sh);
 }
--- src/terminal/delete/delete_right_word.c
+++ src/terminal/delete/delete_right_word.c
@@ -6,22 +6,71 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/27 14:35:55 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/27 15:24:18 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/04 15:11:14 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+static void	sh_move_cursor(t_shell *sh, int pos)
+{
+	int	x;
+	int	y;
+
+	y = (pos + sh->prompt.len_mod) / sh->window.width;
+	x = (y) ? (pos + sh->prompt.len_mod) % sh->window.width : pos;
+	sh_move_home(sh);
+	ft_putstr(K_DEL_ALL);
+	ft_putstr(sh->buffer.content);
+	sh->cursor.y = (sh->buffer.display_len + sh->prompt.len_mod)
+		/ sh->window.width;
+	sh->cursor.x = (sh->cursor.y)
+		? (sh->buffer.display_len + sh->prompt.len_mod) % sh->window.width
+		: sh->buffer.display_len;
+	sh->cursor.abs_pos = sh->buffer.display_len;
+	sh->cursor.rel_pos = sh->buffer.unicode_len;
+	sh->buffer.display_len = ft_strlenu(sh->buffer.content);
+	sh->buffer.unicode_len = ft_strlens(sh->buffer.content);
+	sh_move_to_xy(sh, x, y);
+}
+
+static void	sh_delete_words(t_shell *sh, int rel_start, int rel_stop)
+{
+	int	i;
+
+	i = 0;
+	while (sh->buffer.content[rel_stop + i])
+	{
+		sh->buffer.content[rel_start] =
+		sh->buffer.content[rel_stop + i];
+		rel_start++;
+		i++;
+	}
+	while (sh->buffer.content[rel_start])
+	{
+		sh->buffer.content[rel_start] = '\0';
+		rel_start++;
+	}
+}
+
 void	sh_delete_next_word(t_shell *sh)
 {
+	int		abs_start;
+	int		rel_start;
+	int		abs_stop;
+	int		rel_stop;
+
 	if (sh->cursor.abs_pos >= sh->buffer.display_len)
 		return ;
-	while (sh->cursor.abs_pos < sh->buffer.display_len
-	&& ((unsigned char)sh->buffer.content[sh->cursor.rel_pos] == ' '
-	|| (unsigned char)sh->buffer.content[sh->cursor.rel_pos] == '\t'))
-		sh_delete_current_char(sh);
-	while (sh->cursor.abs_pos < sh->buffer.display_len
-	&& (unsigned char)sh->buffer.content[sh->cursor.rel_pos] != ' '
-	&& (unsigned char)sh->buffer.content[sh->cursor.rel_pos] != '\t')
-		sh_delete_current_char(sh);
+	abs_start = sh->cursor.abs_pos;
+	rel_start = sh->cursor.rel_pos;
+	sh_move_next_word(sh);
+	abs_stop = sh->cursor.abs_pos;
+	rel_stop = sh->cursor.rel_pos;
+	sh->buffer.display_len = sh->buffer.display_len
+		- abs_stop + abs_start;
+	sh->buffer.unicode_len = sh->buffer.unicode_len
+		- rel_stop + rel_start;
+	sh_delete_words(sh, rel_start, rel_stop);
+	sh_move_cursor(sh, abs_start);
 }
