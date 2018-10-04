5eec0acace309868aa7a69c19529e303516efefe
Damien LAURENT-NAUGUET <dlaurent@e1r6p6.42.fr>
2018-10-04 12:14:09 +0200

    [feature] improve speed of selection

--- src/terminal/select/select_to_end.c
+++ src/terminal/select/select_to_end.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/21 16:55:26 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/21 16:56:19 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/04 12:00:33 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,8 +14,15 @@
 
 void	sh_select_end(t_shell *sh)
 {
+	int		i;
+
+	i = 0;
 	if (sh->cursor.abs_pos >= sh->buffer.display_len)
 		return ;
-	while (sh->cursor.abs_pos < sh->buffer.display_len)
-		sh_select_right_char(sh);
+	while (sh->buffer.content[i])
+	{
+		sh->buffer.content[i] = '\0';
+		i++;
+	}
+	ft_putstr(CLEAR_TO_EOL);
 }
--- src/terminal/select/select_to_home.c
+++ src/terminal/select/select_to_home.c
@@ -6,16 +6,47 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/21 16:53:16 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/21 16:55:21 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/04 12:12:06 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-void	sh_select_home(t_shell *sh)
+static void	sh_select_home_reset_params(t_shell *sh, int len, int ulen)
 {
+	sh->buffer.display_len -= len;
+	sh->buffer.unicode_len -= ulen;
+	sh->cursor.y = (sh->prompt.len_mod + sh->buffer.display_len)
+		/ sh->window.width;
+	sh->cursor.x = (sh->cursor.y)
+		? (sh->prompt.len_mod + sh->buffer.display_len) % sh->window.width
+		: sh->buffer.display_len;
+	sh_move_to_xy(sh, 0, 0);
+}
+
+void		sh_select_home(t_shell *sh)
+{
+	int	i;
+	int	j;
+	int	k;
+
+	i = 0;
+	j = sh->cursor.rel_pos;
+	k = sh->cursor.abs_pos;
 	if (sh->cursor.abs_pos == 0)
 		return ;
-	while (sh->cursor.abs_pos)
-		sh_select_left_char(sh);
+	sh_move_home(sh);
+	ft_putstr(CLEAR_TO_EOL);
+	while (sh->buffer.content[i + j])
+	{
+		sh->buffer.content[i] = sh->buffer.content[i + j];
+		i++;
+	}
+	while (sh->buffer.content[i])
+	{
+		sh->buffer.content[i] = '\0';
+		i++;
+	}
+	ft_putstr(sh->buffer.content);
+	sh_select_home_reset_params(sh, k, j);
 }
