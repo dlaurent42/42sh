8540e187f12384e7c88add57916317bb768592ab
davhojt <davhojt@gmail.com>
2018-10-06 09:01:11 +0200

    [refactor] Made inermediate function to control history auto completion.

--- src/terminal/autocompletion/auto_completion/auto_completion.c
+++ src/terminal/autocompletion/auto_completion/auto_completion.c
@@ -6,22 +6,20 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/13 01:28:20 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/06 08:49:31 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/06 08:59:54 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static bool			is_auto_history(t_shell *shell)
+static void			exc_double(t_shell *shell, bool *status)
 {
 	int				offset;;
-	bool			status;
 	char			*track;
 	char			*ptr_to_exc;
 
 	sh_debug(NULL, "Enter History\n", NULL);//DEBUG
 	offset = 0;
-	status = false;
 	while ((ptr_to_exc = ft_strstr(shell->buffer.content + offset, "!!")))//Deal with !! hist
 	{
 		track = shell->buffer.content;
@@ -37,11 +35,26 @@ static bool			is_auto_history(t_shell *shell)
 			sh_delete_current_char(shell);
 			sh_delete_current_char(shell);
 			auto_manage_buffer(shell, shell->cmd->content);
-			status = true;
+			*status = true;
 		}
 		offset++;
 		sh_move_end(shell);
 	}
+}
+
+static void			exc_number(t_shell *shell, bool *status)
+{
+	(void)shell;
+	(void)status;
+}
+
+static bool			auto_history(t_shell *shell)
+{
+	bool			status;
+
+	status = false;
+	exc_double(shell, &status);
+	exc_number(shell, &status);
 	return (status);
 }
 
@@ -105,7 +118,7 @@ void				auto_completion(t_shell *shell)
 	char			*parsed_buffer;
 
 	shell->modes.auto_completion = 1;
-	if (is_auto_history(shell))
+	if (auto_history(shell))
 		;
 	else {
 		ft_bzero(&frame, sizeof(frame));
