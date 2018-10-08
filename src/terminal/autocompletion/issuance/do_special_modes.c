4c8b7c5647371eed3dc9f9f55c3d5e4d9d4f3fd3
davhojt <davhojt@gmail.com>
2018-10-09 01:21:17 +0200

    [feat] gcc now operational

--- src/terminal/autocompletion/issuance/do_special_modes.c
+++ src/terminal/autocompletion/issuance/do_special_modes.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/08 23:59:47 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/09 01:13:57 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/09 01:19:58 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,25 @@
 static bool			gcc_check(char *str)
 {
 	if (!str
-			|| ft_strcmp(str, ".h")
-			|| ft_strcmp(str, ".c")
-			|| ft_strcmp(str, ".i")
-			|| ft_strcmp(str, ".ii")
-			|| ft_strcmp(str, ".cp")
-			|| ft_strcmp(str, ".cc")
-			|| ft_strcmp(str, ".cxx")
-			|| ft_strcmp(str, ".cpp")
-			|| ft_strcmp(str, ".c++")
-			|| ft_strcmp(str, ".C")
-			|| ft_strcmp(str, ".f")
-			|| ft_strcmp(str, ".for")
-			|| ft_strcmp(str, ".FOR")
-			|| ft_strcmp(str, ".F")
-			|| ft_strcmp(str, ".fpp")
-			|| ft_strcmp(str, ".FPP")
-			|| ft_strcmp(str, ".r")
-			|| ft_strcmp(str, ".s")
-			|| ft_strcmp(str, ".S"))
+			|| !ft_strcmp(str, ".h")
+			|| !ft_strcmp(str, ".c")
+			|| !ft_strcmp(str, ".i")
+			|| !ft_strcmp(str, ".ii")
+			|| !ft_strcmp(str, ".cp")
+			|| !ft_strcmp(str, ".cc")
+			|| !ft_strcmp(str, ".cxx")
+			|| !ft_strcmp(str, ".cpp")
+			|| !ft_strcmp(str, ".c++")
+			|| !ft_strcmp(str, ".C")
+			|| !ft_strcmp(str, ".f")
+			|| !ft_strcmp(str, ".for")
+			|| !ft_strcmp(str, ".FOR")
+			|| !ft_strcmp(str, ".F")
+			|| !ft_strcmp(str, ".fpp")
+			|| !ft_strcmp(str, ".FPP")
+			|| !ft_strcmp(str, ".r")
+			|| !ft_strcmp(str, ".s")
+			|| !ft_strcmp(str, ".S"))
 		return (true);
 	return (false);
 }
@@ -47,7 +47,7 @@ static void			filter_obj(t_obj *obj, int filter)
 		if (filter == FILT_CD && !obj->data.dir)
 			obj->data.no_file = 1;
 		if (filter == FILT_GCC
-				&& (obj->data.dir || gcc_check(ft_strrchr(obj->data.str, '.'))))
+				&& (!obj->data.dir && !gcc_check(ft_strrchr(obj->data.str, '.'))))
 			obj->data.no_file = 1;
 		obj = obj->next;
 	}
