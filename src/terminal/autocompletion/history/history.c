21f632739d0273d57985d394f852d7596055693e
davhojt <davhojt@gmail.com>
2018-10-08 08:01:31 +0200

    [feat] Added feature for history string search

--- src/terminal/autocompletion/history/history.c
+++ src/terminal/autocompletion/history/history.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/06 09:05:58 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/08 07:04:15 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/08 08:00:20 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -102,6 +102,49 @@ static void			auto_hist_number(t_shell *shell, bool *status)
 	}
 }
 
+t_cmd				*get_cmd_by_content(t_shell *shell, char *content)
+{
+	int				len;
+	t_cmd			*cmd;
+
+
+	len = ft_strlens(content);
+	cmd = shell->cmd;
+	while (cmd && ft_strcmps(content, cmd->content))
+		cmd = cmd->next;
+	return (cmd);
+}
+
+static void			auto_hist_name(t_shell *shell, bool *status)
+{
+	int				number_of_deletions;
+	int				offset;
+	char			*track;
+	char			*ptr_to_exc;
+	t_cmd			*cmd;
+
+	offset = 0;
+	while ((ptr_to_exc = ft_strstr(shell->buffer.content + offset++, "!")))
+	{
+		if (!ft_isdigit(*(ptr_to_exc + 1)))
+		{
+			if ((cmd = get_cmd_by_content(shell, ptr_to_exc + 1)))
+			{
+				number_of_deletions = ft_strlens(ptr_to_exc);
+				track = shell->buffer.content;
+				sh_move_home(shell);
+				while (track++ != ptr_to_exc)
+					sh_move_right(shell);
+				while (number_of_deletions--)
+					sh_delete_current_char(shell);
+				auto_manage_buffer(shell, cmd->content);
+				*status = true;
+				sh_move_end(shell);
+			}
+		}
+	}
+}
+
 bool				auto_history(t_shell *shell)
 {
 	bool			status;
@@ -109,5 +152,6 @@ bool				auto_history(t_shell *shell)
 	status = false;
 	auto_hist_double(shell, &status);
 	auto_hist_number(shell, &status);
+	auto_hist_name(shell, &status);
 	return (status);
 }
