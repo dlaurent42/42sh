a9b3bb73e88364722c809a8b886cab6616bac60d
davhojt <davhojt@gmail.com>
2018-10-09 22:28:46 +0200

    [refactor] removed bin processing from issuance

--- src/terminal/autocompletion/issuance/issuance.c
+++ src/terminal/autocompletion/issuance/issuance.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/07/20 11:55:05 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/09 22:20:55 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/09 22:27:36 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,19 +16,10 @@
 
 void				auto_issuance(t_shell *sh)
 {
-	if (sh->ac->auto_mode == AUTO_BIN)
-	{
-		if (!(sh->ac->file_name = ft_strdup(sh->ac->argv[1])))
-			return ;
-		auto_show_screen(sh, sh->ac->obj);//Swap out
-	}
-	else
-	{
-		sh->ac->current_obj = sh->ac->obj;
-		if (!auto_get_attributes(sh))
-			return ;
-		sh->ac->obj = sh->ac->current_obj;
-		if (sh->ac->obj->data.dir && !sh->ac->obj->data.no_file)
-			auto_do_ls(sh, sh->ac->obj);
-	}
+	sh->ac->current_obj = sh->ac->obj;
+	if (!auto_get_attributes(sh))
+		return ;
+	sh->ac->obj = sh->ac->current_obj;
+	if (sh->ac->obj->data.dir && !sh->ac->obj->data.no_file)
+		auto_do_ls(sh, sh->ac->obj);
 }
