44a18180fb9932a472b7b81c95445967941111f5
davhojt <davhojt@gmail.com>
2018-10-10 08:21:54 +0200

    [refactor] modified sort order.

--- src/terminal/autocompletion/sort/sort.c
+++ src/terminal/autocompletion/sort/sort.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/18 19:26:22 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/08 08:43:52 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/10 08:20:56 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,42 +14,43 @@
 #include "auto_completion.h"
 #include "auto_completion_prot.h"
 
-static bool			swap(t_shell *sh)
+bool				sort_alpha(t_obj *track)
+{
+	if (ft_strcmp(track->data.str, track->next->data.str) > 0)
+		return (true);
+	return (false);
+}
+
+static bool			swap(t_obj *track)
 {
 	t_data			tmp;
 
-	if (auto_sort_alpha(sh))
+	if (sort_alpha(track))
 	{
-		tmp = sh->ac->track->data;
-		sh->ac->track->data = sh->ac->track->next->data;
-		sh->ac->track->next->data = tmp;
+		tmp = track->data;
+		track->data = track->next->data;
+		track->next->data = tmp;
 		return (true);
 	}
 	return (false);
 }
 
-static void			do_sort(t_shell *sh)
+void				auto_sort(t_obj *obj)
 {
 	bool			sorted;
+	t_obj			*track;
 
 	while (true)
 	{
-		sh->ac->track = sh->ac->head;
+		track = obj;
 		sorted = true;
-		while (sh->ac->track && sh->ac->track->next)
+		while (track && track->next)
 		{
-			if (swap(sh))
+			if (swap(track))
 				sorted = false;
-			sh->ac->track = sh->ac->track->next;
+			track = track->next;
 		}
 		if (sorted)
 			break ;
 	}
-}
-
-void				auto_sort(t_shell *sh)
-{
-	sh->ac->head = sh->ac->current_obj;
-	do_sort(sh);
-	sh->ac->current_obj = sh->ac->head;
 }
