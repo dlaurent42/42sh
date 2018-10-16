36b844849bcb96707ff9a9e62e1362a7a76243b9
Dav Højt <davhojt@gmail.com>
2018-10-16 08:24:41 +0200

    [feat] Now gets confirms position of occurance of 'needle' in cmd->content

--- src/terminal/autocompletion/history/hist_sed.c
+++ src/terminal/autocompletion/history/hist_sed.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/15 19:34:33 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/15 20:37:32 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/16 08:21:48 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,49 +40,71 @@ static bool			content_is_hist_sed(t_shell *sh, char *content)
 	char			*chevron;
 
 	status = true;
-	if (*content && *content == '^')
+	if (status && (chevron = ft_strrchr(content, '^')) && *(chevron + 1))
+		do_error(sh, &status);
+	number_of_chevrons = 0;
+	while (status && *content && number_of_chevrons < 4)
 	{
-		if (status && (chevron = ft_strrchr(content, '^')) && *(chevron + 1))
+		if (*content == '^' && *(content + 1) == '^')
 			do_error(sh, &status);
-		number_of_chevrons = 0;
-		while (status && *content && number_of_chevrons < 4)
-		{
-			if (*content == '^' && *(content + 1) == '^')
-				do_error(sh, &status);
-			if (*content == '^')
-				number_of_chevrons++;
-			content++;
-		}
-		if (number_of_chevrons != 3)
-			do_error(sh, &status);
+		if (*content == '^')
+			number_of_chevrons++;
+		content++;
 	}
+	if (number_of_chevrons != 3)
+		do_error(sh, &status);
 	return (status);
 }
 
-void				auto_hist_sed(t_shell *sh, bool *status)
+char				*content(t_shell *sh)
+{
+	return (sh->buffer.content + sh->buffer.ushift);
+}
+
+char				*get_pointer_to_needle_in_cmd(t_shell *sh)
+{
+	char			*needle;
+	char			*ptr_to_matched_char_in_cmd;
+
+	needle = NULL;
+	ptr_to_matched_char_in_cmd = NULL;
+	if (sh->cmd && (needle = ft_strndup((content(sh) + 1),
+					ft_strchr((content(sh) + 1), '^') - (content(sh) + 1))))
+		ptr_to_matched_char_in_cmd = ft_strstr(sh->cmd->content, needle);
+	free(needle);
+	return (ptr_to_matched_char_in_cmd);
+}
+
+void				auto_hist_sed(t_shell *sh, bool *status)//make bool
 {
 	int				number_of_deletions;
 	char			*track;
 	char			*ptr_to_exc;
 	t_cmd			*cmd;
 
-	if (!content_is_hist_sed(sh, sh->buffer.content + sh->buffer.ushift))
-		return ;
-	while ((ptr_to_exc = ft_strstr(sh->buffer.content + sh->buffer.ushift, "!")))
+	if (*content(sh) && *content(sh) == '^')
 	{
-		if (!ft_isdigit(*(ptr_to_exc + 1))
-				&& ((cmd = get_cmd_by_content(sh, ptr_to_exc + 1))))
-		{
-			number_of_deletions = ft_strlens(ptr_to_exc);
-			track = sh->buffer.content + sh->buffer.ushift;
-			sh_move_home(sh);
-			while (track++ != ptr_to_exc)
-				sh_move_right(sh);
-			while (number_of_deletions--)
-				sh_delete_current_char(sh);
-			sh_print_str(sh, cmd->content);
-			*status = true;
-			sh_move_end(sh);
-		}
+		*status = true;
+		if (!content_is_hist_sed(sh, content(sh)))
+			return ;
+		if (!(ft_printf("%s\n", get_pointer_to_needle_in_cmd(sh))))
+			return ;
+			while ((ptr_to_exc = ft_strstr(sh->buffer.content + sh->buffer.ushift, "!")))
+			{
+				if (!ft_isdigit(*(ptr_to_exc + 1))
+						&& ((cmd = get_cmd_by_content(sh, ptr_to_exc + 1))))
+				{
+					number_of_deletions = ft_strlens(ptr_to_exc);
+					track = sh->buffer.content + sh->buffer.ushift;
+					sh_move_home(sh);
+					while (track++ != ptr_to_exc)
+						sh_move_right(sh);
+					while (number_of_deletions--)
+						sh_delete_current_char(sh);
+					sh_print_str(sh, cmd->content);
+					*status = true;
+					sh_move_end(sh);
+				}
+			}
 	}
 }
