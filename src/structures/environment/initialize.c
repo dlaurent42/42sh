5c806be66c48dc01e633ef0b78f0ec2a6dd21917
dlaurent <dlaurent@student.42.fr>
2018-10-17 14:23:49 +0200

    hash table securisation

--- src/structures/environment/initialize.c
+++ src/structures/environment/initialize.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/30 19:20:06 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/16 19:31:27 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/17 13:48:26 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,7 +21,7 @@ void		env_initialize(t_shell *sh, t_env *env, char **environ)
 	i = 0;
 	key = NULL;
 	value = NULL;
-	while (environ && environ[i])
+	while (environ && environ[i] && i < ENV_MAX_SIZE)
 	{
 		value = ft_strchrsp(environ[i], '=');
 		key = ft_strsub(environ[i], 0,
