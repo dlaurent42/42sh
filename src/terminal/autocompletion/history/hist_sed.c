8c8df5ffc6ea9c712bc55b0345bb99c8b828a2f3
Dav Højt <davhojt@gmail.com>
2018-10-17 23:18:23 +0200

    [feat] Refactored order towords norm

--- src/terminal/autocompletion/history/hist_sed.c
+++ src/terminal/autocompletion/history/hist_sed.c
@@ -6,54 +6,42 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/15 19:34:33 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/17 22:00:41 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/17 23:16:22 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static void			do_error(t_shell *sh, bool *status)
-{
-	if (sh)
-		;
-	//if (*status)
-	//	ft_putendl("ERROR");/////////////////////////////////
-	*status = false;
-}
-
-static bool			content_is_hist_sed(t_shell *sh, char *content)
+static bool			content_is_hist_sed(char *content)
 {
 	int				number_of_chevrons;
-	bool			status;
+	bool			is_sed;
 	char			*chevron;
 
-	status = true;
-	if (status && (chevron = ft_strrchr(content, '^')) && *(chevron + 1))
-		do_error(sh, &status);
+	is_sed = true;
+	if (is_sed && (chevron = ft_strrchr(content, '^')) && *(chevron + 1))
+		is_sed = false;
 	number_of_chevrons = 0;
-	while (status && *content && number_of_chevrons < 4)
+	while (is_sed && *content && number_of_chevrons < 4)
 	{
 		if (*content == '^' && *(content + 1) == '^')
-			do_error(sh, &status);
+			is_sed = false;
 		if (*content == '^')
 			number_of_chevrons++;
 		content++;
 	}
 	if (number_of_chevrons != 3)
-		do_error(sh, &status);
-	return (status);
+		is_sed = false;
+	return (is_sed);
 }
 
-char				*content(t_shell *sh)
-{
-	return (sh->buffer.content + sh->buffer.ushift);
-}
-
-char				*get_substitution(t_shell *sh)
+static char				*get_substitution(t_shell *sh)
 {
 	char			*needle;
+	char			*content;
 
-	needle = ft_strchr(content(sh), '^');
+	content = sh->buffer.content + sh->buffer.ushift;
+	needle = ft_strchr(content, '^');
 	if (needle && *needle)
 		needle++;
 	if (needle && *needle)
@@ -65,57 +53,60 @@ char				*get_substitution(t_shell *sh)
 
 static void				get_needles(t_shell *sh, char **needle, char **needle_in_content)
 {
+	char			*content;
+
+	content = sh->buffer.content + sh->buffer.ushift;
 	*needle = NULL;
 	*needle_in_content = NULL;
-	if (sh->cmd && (*needle = ft_strndup((content(sh) + 1),
-					ft_strchr((content(sh) + 1), '^') - (content(sh) + 1))))
+	if (sh->cmd && (*needle = ft_strndup((content + 1),
+					ft_strchr((content + 1), '^') - (content + 1))))
 		*needle_in_content = ft_strstr(sh->cmd->content, *needle);
 }
 
+/*
+** strings[0] == needle
+** strings[1] == needle_in_content
+** strings[3] == substitution
+*/
+
+static void			exchange_cmd(t_shell *sh, char **strings)
+{
+	char			*track;
+	int				number_of_deletions;
+
+	sh_delete_all(sh);
+	sh_print_str(sh, sh->cmd->content);
+	sh_move_home(sh);
+	track = sh->cmd->content;
+	while (track++ != strings[1])
+		sh_move_right(sh);
+	number_of_deletions = ft_strlens(strings[0]);
+	while (number_of_deletions--)
+		sh_delete_current_char(sh);
+	sh_print_str(sh, strings[2]);
+	sh_move_end(sh);
+}
+
+
 void				auto_hist_sed(t_shell *sh, bool *status)//make bool
 {
-	int				number_of_deletions;
-	char			*track;
-	char			*needle;//free it
-	char			*needle_in_content;
-	char			*substitution;
+	char			**strings;
+	char			*content;
 
-	if (*content(sh) && *content(sh) == '^')
+	content = sh->buffer.content + sh->buffer.ushift;
+	if (*content && *content == '^')
 	{
 		*status = true;
-		if (!content_is_hist_sed(sh, content(sh)))
+		if (!content_is_hist_sed(content))
 			return ;
-		get_needles(sh, &needle, &needle_in_content);
-		if (!needle || !needle_in_content)//free both
+		if (!(strings = (char **)malloc(sizeof(char *) * 3)))
 			return ;
-		if (!(substitution = get_substitution(sh)))
-			return ;
-		sh_delete_all(sh);
-		sh_print_str(sh, sh->cmd->content);
-		sh_move_home(sh);
-		track = sh->cmd->content;
-		while (track++ != needle_in_content)
-			sh_move_right(sh);
-		number_of_deletions = ft_strlens(needle);
-		while (number_of_deletions--)
-			sh_delete_current_char(sh);
-		sh_print_str(sh, substitution);
-		/*
-			while ((ptr_to_exc = ft_strstr(sh->buffer.content + sh->buffer.ushift, "!")))
-			{
-				{
-					number_of_deletions = ft_strlens(ptr_to_exc);
-					track = sh->buffer.content + sh->buffer.ushift;
-					sh_move_home(sh);
-					while (track++ != ptr_to_exc)
-						sh_move_right(sh);
-					while (number_of_deletions--)
-						sh_delete_current_char(sh);
-					sh_print_str(sh, cmd->content);
-					*status = true;
-					sh_move_end(sh);
-				}
-			}
-			*/
+		get_needles(sh, &strings[0], &strings[1]);
+		strings[2] = get_substitution(sh);
+		if (strings[0] && strings[1] && strings[2])
+			exchange_cmd(sh, strings);
+		free(strings[0]);
+		free(strings[2]);
+		free(strings);
 	}
 }
