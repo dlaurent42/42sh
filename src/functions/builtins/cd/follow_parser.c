545698b3f068a05024d6f2bb02bd94bcadcaeb4a
dlaurent <dlaurent@student.42.fr>
2018-10-17 22:26:40 +0200

    fix segfault on cd

--- src/functions/builtins/cd/follow_parser.c
+++ src/functions/builtins/cd/follow_parser.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/29 00:46:36 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/09/29 01:09:28 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/17 22:26:07 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -31,7 +31,7 @@ int			sh_cd_remove_troll(char *s)
 	int	i;
 
 	i = 0;
-	while (s[i] && s[i] == '.' && (s[i + 1] == '\0' || s[i + 1] == '/'))
+	while (s && s[i] && s[i] == '.' && (s[i + 1] == '\0' || s[i + 1] == '/'))
 	{
 		i++;
 		if (s[i] && s[i] == '/')
@@ -42,13 +42,13 @@ int			sh_cd_remove_troll(char *s)
 
 static char	*sh_cd_remove_dot_dot(char *s, int i, int j)
 {
-	while (s[j] && s[i + 3])
+	while (s && s[j] && s[i] && s[i + 1] && s[i + 2] && s[i + 3])
 	{
 		s[j] = s[i + 3];
 		i++;
 		j++;
 	}
-	while (s[j])
+	while (s && s[j])
 	{
 		s[j] = '\0';
 		j++;
@@ -58,12 +58,12 @@ static char	*sh_cd_remove_dot_dot(char *s, int i, int j)
 
 static char	*sh_cd_remove_dot(char *s, int i)
 {
-	while (s[i + 2])
+	while (s && s[i] && s[i + 1] && s[i + 2])
 	{
 		s[i] = s[i + 2];
 		i++;
 	}
-	while (s[i])
+	while (s && s[i])
 	{
 		s[i] = '\0';
 		i++;
@@ -77,10 +77,9 @@ char		*sh_cd_parse_path(char *s)
 	int	last_slash;
 
 	i = 0;
-	while (s[i])
-		if (s[i] == '/' && s[i + 1] != '.')
-			last_slash = i++;
-		else if (s[i] == '/' && s[i + 1] == '.' && s[i + 2] == '.'
+	last_slash = -1;
+	while (s && s[i])
+		if (s[i] == '/' && s[i + 1] == '.' && s[i + 2] == '.'
 		&& (s[i + 3] == '/' || s[i + 3] == '\0'))
 		{
 			s = sh_cd_remove_dot_dot(s, i, last_slash);
@@ -92,6 +91,8 @@ char		*sh_cd_parse_path(char *s)
 			s = sh_cd_remove_dot(s, i);
 			i = 0;
 		}
+		else if (s[i] == '/')
+			last_slash = i++;
 		else
 			i++;
 	return (s);
