984b5112c44719348cdc9e56ff47a695bde00bbd
dlaurent <dlaurent@student.42.fr>
2018-10-17 23:49:25 +0200

    Ctrl + D behavior

--- src/terminal/read/read.c
+++ src/terminal/read/read.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/01 16:10:01 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/16 19:17:43 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/17 23:48:55 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,8 +19,10 @@ void		sh_read(t_shell *sh)
 	{
 		if (read(0, sh->read->line, LINE_SIZE - 1) == -1)
 			break ;
-		if (sh->read->line[0] == 4
-		&& !sh->modes.multiline)
+		if (sh->read->line[0] == 4 && !sh->modes.multiline
+		&& sh->buffer.display_len + sh->buffer.dshift)
+			sh_delete_current_char(sh);
+		else if (sh->read->line[0] == 4 && !sh->modes.multiline)
 			break ;
 		sh_read_dispatcher(sh);
 		ft_bzero(sh->read->line, LINE_SIZE);
