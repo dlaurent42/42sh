97df44f126aa6a974614cdd5eeb38dc18611ab6f
dlaurent <dlaurent@student.42.fr>
2018-10-22 16:42:57 +0200

    [feature] globing pattern matching

--- src/functions/glob/glob.c
+++ src/functions/glob/glob.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/18 19:51:26 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/22 16:40:35 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/22 16:42:27 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,14 +26,13 @@
 **			[ab]	can match a or b
 **			[a-d]	can match a, b, c or d
 **			[!a-d]	cannot match a, b, c or d
-**			[^a-d]	??????????????????????
 **		{}	Curly braces
-**			ok-{1..9}	   -> 1 2 3 4 5 6 7 8 9
-**			ok-{a,b}	   -> a b
+**			{1..9}	   -> 1 2 3 4 5 6 7 8 9
+**			{a,b}	   -> a b
 **			{{A,b}..2} -> \{A..2\} \{b..2\}
-**			ok-{{A..b},2} -> A B C D E F G H I J K L M NO P Q R S T U V W X Y
+**			{{A..b},2} -> A B C D E F G H I J K L M NO P Q R S T U V W X Y
 **						  Z \[ \\ \] \^ _ \` a b 2
-**			ok-{{A,b},c} -> A b c
+**			{{A,b},c} -> A b c
 */
 
 #include "shell.h"
