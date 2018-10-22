ca1f7c0ddef94962d5fcfbf8df65ae41042d5ffc
dlaurent <dlaurent@student.42.fr>
2018-10-22 16:48:32 +0200

    [feature] globing debug possibility from main

--- src/shell.c
+++ src/shell.c
@@ -6,36 +6,43 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/24 00:41:06 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/22 15:01:29 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/22 16:46:53 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-int		main(int argc, char **argv, char **environ)
+static int	debug_globbing(char *argument)
 {
-	t_shell		*sh;
+	int		i;
+	char	*str;
 
-	sh = NULL;
-	(void)argc;
-	// (void)argv;
-	// sh = sh_new(environ);
-	// sh_welcome();
-	// signal_catching();
-	// sh_read(sh);
-	// sh_delete(sh);
-	(void)environ;
-	char	*str = ft_strdups(argv[1]);
-	ft_printf("initial: %s\n", str);
+	i = 0;
+	str = ft_strdups(argument);
+	ft_printf("Received argument: %s\n", str);
 	str = sh_glob(str);
-	int		i = 0;
-	ft_printf("final  : [");
+	ft_printf("Returned argument: ");
 	while (str[i])
 	{
 		ft_putchar(str[i]);
 		i++;
 	}
-	ft_printf("]\n");
+	ft_printf("\n");
 	ft_strdel(&str);
+	return (EXIT_SUCCESS);
+}
+
+int			main(int argc, char **argv, char **environ)
+{
+	t_shell		*sh;
+
+	sh = NULL;
+	if (argc > 1)
+		return (debug_globbing(argv[1]));
+	sh = sh_new(environ);
+	sh_welcome();
+	signal_catching();
+	sh_read(sh);
+	sh_delete(sh);
 	return (EXIT_SUCCESS);
 }
