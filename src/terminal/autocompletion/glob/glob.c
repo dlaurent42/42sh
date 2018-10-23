44cead1b30686f3197f2c1160290eed1179a3267
Dav Højt <davhojt@gmail.com>
2018-10-23 08:45:52 +0200

    [feat] Globbing function complete. Need to investigate infinite loop in sh_print_str.

--- src/terminal/autocompletion/glob/glob.c
+++ src/terminal/autocompletion/glob/glob.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <dhojt@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/22 23:52:08 by dhojt             #+#    #+#             */
-/*   Updated: 2018/10/23 00:02:03 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/23 08:44:02 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,19 +14,21 @@
 
 bool				replace_buffer_content(t_shell *sh, char *str, char *glob, char *ptr_to_content)
 {
-	char			*content;
+	char			*track;
 	int				number_of_deletions;
 
 	if (!ft_strcmps(str, glob))
 		return (false);
-	number_of_deletions = ft_strlens(glob);
-	content = sh->buffer.content + sh->buffer.ushift;
+	number_of_deletions = ft_strlens(str);
+	track = sh->buffer.content + sh->buffer.ushift;
 	sh_move_home(sh);
-	while (content++ != ptr_to_content)
+	while (track++ != ptr_to_content)
 		sh_move_right(sh);
 	while (number_of_deletions--)
 		sh_delete_current_char(sh);
 	sh_print_str(sh, glob);
+	sh_print_str(sh, " ");
+	sh_move_end(sh);
 	return (true);
 }
 
@@ -41,8 +43,10 @@ bool				auto_glob(t_shell *sh)
 	glob = NULL;
 	status = false;
 	if (ft_strcmps(sh->read->line, K_TAB))
-		return (false);
-	if(!(ptr_to_content = ft_strrchr(sh->buffer.content + sh->buffer.ushift, ' ')))
+		return (status);
+	if((ptr_to_content = ft_strrchr(sh->buffer.content + sh->buffer.ushift, ' ')))
+		ptr_to_content++;
+	else
 		ptr_to_content = sh->buffer.content + sh->buffer.ushift;
 	str = ft_strdups(ptr_to_content);
 	glob = sh_glob(ft_strdup(str));
