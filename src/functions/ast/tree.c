fca35b48b51be7125f34ebb00c6330b99067048f
Adomas ZALIAUSKAS <azaliaus@student.42.fr>
2018-10-24 16:03:34 +0200

    some cleaning up.

--- src/functions/ast/tree.c
+++ src/functions/ast/tree.c
@@ -6,20 +6,18 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 20:30:34 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/10/24 14:16:34 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/10/24 15:58:50 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-void		sh_build_tree(t_shell *sh, t_env *env, t_bin *bin, char *cmd)
+void		sh_build_tree(t_shell *sh, t_env *env, t_bin *bin,
+										char *cmd)
 {
 	t_lexer			lexer;
 	t_token_tree	*list;
 
-	(void)sh;
-	(void)env;
-	(void)bin;
 	if (!cmd)
 		return ;
 	lexer = lexer_entry(cmd);
@@ -27,6 +25,7 @@ void		sh_build_tree(t_shell *sh, t_env *env, t_bin *bin, char *cmd)
 	lexer_delete(&lexer);
 	if (!(list = build_token_tree(list))) // IDEA: error message could be nice.
 		return ;
-
+	// TODO: Reorganise tree(prioritising)
+	execute_tree(sh, env, bin, list); // returns last return code
 	clean_tree(list);
 }
