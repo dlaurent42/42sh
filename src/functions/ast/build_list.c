ddf649269c621947e77c3506f5d4e2f46ab9b3cc
Adomas ZALIAUSKAS <azaliaus@student.42.fr>
2018-10-24 11:50:42 +0200

    List builder remake[not done]

--- src/functions/ast/build_list.c
+++ src/functions/ast/build_list.c
@@ -6,16 +6,87 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 21:08:41 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/10/23 21:38:34 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/10/24 11:49:44 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
+
+#include "shell.h"
+
+/*
+**	IDEA: remake it, to make less bulky
+*/
+static bool			tree_splice_point(t_lexer lexer, size_t i)
+{
+	if (lexer.tokens[i].type == TOKEN_ANDIF ||
+		lexer.tokens[i].type == TOKEN_AGGREG ||
+		lexer.tokens[i].type == TOKEN_ORIF ||
+		lexer.tokens[i].type == TOKEN_REDIR ||
+		lexer.tokens[i].type == TOKEN_SEMICOLON ||
+		lexer.tokens[i].type == TOKEN_PIPE ||
+		lexer.tokens[i].type == TOKEN_REDIR)
+		return (true);
+	return (false);
+}
+
+static void			tree_group_token(t_lexer lexer, t_token_tree **head,
+										size_t len, size_t begin)
+{
+	t_token_tree		*new;
+	size_t				i;
+
+	if (len == 0)
+		return ;
+	new = new_tree_node();
+	new->size = len;
+	new->type = 0;
+	new->tokens = (char **)ft_memalloc(sizeof(char *) * len); // protect later on
+	i = 0;
+	while (i < len)
+	{
+		new->tokens[i] = ft_strdup(lexer.tokens[i + begin].id);
+		i++;
+	}
+	add_tree_to_back(head, new);
+
+}
 
 t_token_tree		*build_list(t_lexer lexer)
 {
 	t_token_tree		*head;
-	t_token_tree		*last;
+	size_t				len;
+	size_t				i;
 
 	head = NULL;
-	last = NULL;
+	len = 0;
+	i = 0;
+	// will be removed
+	for (size_t x = 0; x < lexer.size; x++)
+		printf("%zu: token: %s | type: %d\n", x, lexer.tokens[x].id, lexer.tokens[x].type);
+	// /will be removed
+	while (i < lexer.size)
+	{
+		if (tree_splice_point(lexer, i))
+		{
+			printf("Found sperator! Len: %zu | From: %zu\n", len, i - len);
+			tree_group_token(lexer, &head, len, i - len);
+			tree_group_token(lexer, &head, 1, i);
+			i++;
+			len = 0;
+		}
+		else
+			len++;
+		i++;
+	}
+	if (len > 0)
+		tree_group_token(lexer, &head, len, i - len);
+
+	while (head)
+	{
+		printf("List: size: %zu | Elements: ", head->size);
+		for (size_t i = 0; i < head->size; i++)
+			printf("%s,", head->tokens[i]);
+		printf("\n");
+		head = head->next;
+	}
 	return (head);
 }
