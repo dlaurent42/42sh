59db70b70761b64494019d87f3c72528bfea6e74
dlaurent <dlaurent@student.42.fr>
2018-10-24 12:58:43 +0200

    [feature] lexer reorga

--- src/functions/lexer/heredoc/heredoc.c
+++ src/functions/lexer/heredoc/heredoc.c
@@ -6,11 +6,21 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/23 22:12:01 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/24 12:52:26 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/24 12:58:34 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+/*
+** sh_heredoc expect heredoc to be malloced and valid
+**
+** examples:
+**		cat<<toto	->	heredoc = toto
+**		cat << toto	->	heredoc = toto
+**		cat <<		-> 	function not called
+**		cat <<>		->	function not called
+*/
 
 char		sh_heredoc(t_shell *sh, char *heredoc)
 {
