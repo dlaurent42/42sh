bd5961eb19a5a54281e33901c4d1d6cad111f92e
dlaurent <dlaurent@student.42.fr>
2018-10-24 14:21:03 +0200

    [feature] heredoc(s)

--- src/functions/lexer/heredoc/update.c
+++ src/functions/lexer/heredoc/update.c
@@ -6,13 +6,13 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/24 14:02:56 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/24 14:07:43 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/24 14:20:40 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static char	sh_heredoc_update_close(t_shell *sh, t_heredocs *heredocs)
+static char	sh_heredoc_update_close(t_heredocs *heredocs)
 {
 	heredocs->closed = TRUE;
 	if (!heredocs->next)
@@ -31,7 +31,7 @@ char		sh_heredoc_update(t_shell *sh)
 	if (!tmp)
 		return (-1);
 	if (ft_strcmps(sh->buffer.parsed + sh->buffer.ushift, tmp->keyword) == 0)
-		return (sh_heredoc_update_close(sh, tmp));
+		return (sh_heredoc_update_close(tmp));
 	tmp->keyword = ft_strjoinf(
 		tmp->keyword,
 		sh->buffer.content + sh->buffer.ushift,
