b628fe572dfdce2039300de56b4ec83aeae1106a
Adomas ZALIAUSKAS <azaliaus@student.42.fr>
2018-10-26 11:29:21 +0200

    Updated TODO.

--- src/functions/ast/operators/file_descriptor.c
+++ src/functions/ast/operators/file_descriptor.c
@@ -6,13 +6,18 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 10:34:55 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/10/26 10:36:55 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/10/26 10:42:45 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
+/*
+** IDEA: default file descriptor aggegation is 1, if first node exceeds 2 digits
+*/
 char		execute_fd_aggr(t_shell *sh, t_token_tree *tree)
 {
-	
+	(void)sh;
+	(void)tree;
+	return (0);
 }
