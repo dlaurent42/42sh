200ad41e81dea1a10d25c4708119cbb4072c110f
Dav HOJT <davhojt@gmail.com>
2018-10-27 21:27:00 +0200

    [test] Added test.

--- src/functions/ast/reorganiser.c
+++ src/functions/ast/reorganiser.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 22:54:42 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/10/27 20:53:11 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/27 21:25:55 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,23 +27,24 @@ static t_token_tree	*get_next_token(t_token_tree *token)
 	t_token_tree	*next_token;
 
 	next_token = token;
-	//Put some code here probably.
+	if (!token_is_treatable(next_token))
+		next_token = next_token->right;
 	while (next_token && token_is_treatable(next_token))
 		next_token = next_token->right;
 	return (next_token);
 }
 
-//static void			insert_token_at_correct_place(
-//		t_token_tree *current_command,
-//		t_token_tree *current_token,
-//		t_token_tree *next_token)
-//{
-//	while (current_command && get_next_token(current_command)
-//			&& get_next_token(current_command)->right)
-//		current_command = get_next_token(current_command);
-//	if (current_token || current_command || next_token)
-//		;
-//}
+static void			insert_token_at_correct_place(
+		t_token_tree *current_command,
+		t_token_tree *current_token,
+		t_token_tree *next_token)
+{
+	while (current_command && get_next_token(current_command)
+			&& get_next_token(current_command)->right)
+		current_command = get_next_token(current_command);
+	if (current_token || current_command || next_token)
+		;
+}
 
 static void			treat_current_command(t_token_tree *current_command)
 {
@@ -59,8 +60,8 @@ static void			treat_current_command(t_token_tree *current_command)
 		if (current_token->type < type)
 		{
 			next_token = get_next_token(current_token);
-			//insert_token_at_correct_place(
-			//		current_command, current_token, next_token);
+			insert_token_at_correct_place(
+					current_command, current_token, next_token);
 			current_token = next_token;
 		}
 		current_token = (current_token) ? current_token->right : current_token;
@@ -91,12 +92,15 @@ int					reorganise_tokens(t_token_tree **list)
 {
 	t_token_tree	*current_command;
 
+	ft_printf("\n----START REORGANISE----\n\n");
 	current_command = *list;
 	assign_token_ids(current_command);
 	while (current_command)
 	{
+		ft_printf("Current command [%s]\n", *current_command->tokens);
 		treat_current_command(current_command);
 		move_to_next_command(&current_command);
 	}
+	ft_printf("\n----END REORGANISE----\n");
 	return (1);
 }
