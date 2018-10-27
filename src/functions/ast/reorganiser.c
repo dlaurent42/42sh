999b4d9bbfe14da85c98a41293ea68a85732dca0
Dav HOJT <davhojt@gmail.com>
2018-10-27 17:56:51 +0200

    [feat] Added format to parse entire list

--- src/functions/ast/reorganiser.c
+++ src/functions/ast/reorganiser.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 22:54:42 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/10/27 17:34:01 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/27 17:55:45 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -55,6 +55,27 @@
 //	return (true);
 //}
 
+static bool			token_is_treatable(t_token_tree *current_token)
+{
+	if (!current_token
+			|| current_token->type == 4
+			|| current_token->type == 5
+			|| current_token->type == 6)
+		return (false);
+	return (true);
+}
+
+static void			treat_current_command(t_token_tree *head)
+{
+	t_token_tree	*current_token;
+
+	current_token = head;
+	while (token_is_treatable(current_token))
+	{
+		current_token = current_token->right;
+	}
+}
+
 static void			assign_token_ids(t_token_tree *list)
 {
 	int				i;
@@ -69,8 +90,18 @@ static void			assign_token_ids(t_token_tree *list)
 
 int					reorganise_tokens(t_token_tree **list)
 {
-	(void)list;
+	t_token_tree	*current_command;
+
+	current_command = *list;
 	assign_token_ids(*list);
+	while (current_command && !token_is_treatable(current_command))
+	{
+		treat_current_command(current_command);
+		while (current_command && !token_is_treatable(current_command))
+		{
+			current_command = current_command->right;
+		}	
+	}
 	// reogranisation(list);
 	// arrange_nodes_in_priority(list);
 	return (1);
