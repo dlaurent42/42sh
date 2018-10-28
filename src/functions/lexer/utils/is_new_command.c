4a6d97a90efdd79b5cbbc4d1f923f431ebdb3cf2
dlaurent <dlaurent@student.42.fr>
2018-10-28 16:01:38 +0100

    merging

--- src/functions/lexer/utils/is_new_command.c
+++ src/functions/lexer/utils/is_new_command.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 13:56:30 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/28 14:58:24 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/10/28 15:59:50 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,7 +20,7 @@ bool		lexer_is_new_cmd(char *s, int pos)
 	while (i >= 0)
 	{
 		if ((s[i] == '|' || s[i] == '&' || s[i] == ';' || s[i] == '`')
-		&& lexer_is_esc(s, i))
+		&& !lexer_is_esc(s, i))
 			return (TRUE);
 		else if (s[i] != ' ' || lexer_is_esc(s, i))
 			return (FALSE);
