97dfe6057079369c877b108f4bbcc538dd32277d
Dav HOJT <davhojt@gmail.com>
2018-10-28 19:38:04 +0100

    [refactor] split big function

--- src/functions/ast/reorganiser.c
+++ src/functions/ast/reorganiser.c
@@ -6,34 +6,23 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 22:54:42 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/10/28 19:09:49 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/10/28 19:37:40 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static char			**get_new_token_array(t_token_tree *current_command)
+static char			**get_new_token_array(t_token_tree *current_command,
+		int size)
 {
-	int				size;
 	char			**tokens;
 	char			**tokens_head;
 	char			**track_tokens;
 	t_token_tree	*track_command;
 
-	size = 0;
-	track_command = current_command;
-	//CALCULATE SIZE OF ARRAY
-	while (track_command && track_command->type == 0)
-	{
-		track_tokens = track_command->tokens;
-		while (*track_tokens && ++size)
-			track_tokens++;
-		track_command = track_command->right;
-	}
 	if (!(tokens = (char **)ft_memalloc(sizeof(char *) * (size + 1))))
 		return (NULL);
 	tokens_head = tokens;
-	//FILL NEW ARRAY:
 	track_command = current_command;
 	while (track_command && track_command->type == 0)
 	{
@@ -53,16 +42,27 @@ static char			**get_new_token_array(t_token_tree *current_command)
 static char			**squash_type_zero(t_token_tree *current_command)
 {
 	int				len;
-	char			**tokens;
+	int				size;
+	char			**track_tokens;
 	t_token_tree	*track;
 
 	len = 0;
-	tokens = NULL;//Need this?
+	size = 0;
 	track = current_command;
 	while (track && track->type == 0 && ++len)
 		track = track->right;
 	if (len > 1)
-		return (get_new_token_array(current_command));
+	{
+		track = current_command;
+		while (track && track->type == 0)
+		{
+			track_tokens = track->tokens;
+			while (*track_tokens && ++size)
+				track_tokens++;
+			track = track->right;
+		}
+		return (get_new_token_array(current_command, size));
+	}
 	return (current_command->tokens);
 }
 
@@ -70,8 +70,8 @@ static bool			token_is_boundry(t_token_tree *token)
 {
 	if (!token
 			|| (token->type != 4
-			&& token->type != 5
-			&& token->type != 6))
+				&& token->type != 5
+				&& token->type != 6))
 		return (false);
 	return (true);
 }
