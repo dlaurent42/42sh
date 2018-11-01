33b4eb345f9bd45b252531c5d813cb4dbd7bdb1b
dlaurent <dlaurent@student.42.fr>
2018-11-01 15:46:38 +0100

    fuck it

--- src/functions/run.lexer.c
+++ src/functions/run.lexer.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 22:29:23 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/01 14:37:33 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/01 15:30:08 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,17 +19,17 @@ char		sh_command_run_lexer(
 
 	(void)env;
 	ft_bzero((void *)lexer, sizeof(t_lexer));
-	if (sh->modes.heredoc == FALSE && lexer_is_empty(*cmd))
-		return (STATUS_EMPTY);
 	if ((status = brackets(*cmd, '\0')) != STATUS_OK)
 		;
+	if (sh->modes.heredoc == FALSE && lexer_is_empty(*cmd))
+		return (STATUS_EMPTY);
 	else if (sh_heredocs_all_close(sh))
 		status = lexer_fill(lexer, *cmd);
 	else
 	{
 		if ((status = sh_heredoc(sh, NULL)) == STATUS_OK)
 		{
-			*cmd = sh->buffer.parsed;
+			cmd = &sh->buffer.parsed;
 			status = lexer_fill(lexer, *cmd);
 		}
 	}
