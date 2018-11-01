0b80ab4f82c2b9ec4840c24f7c7337d9bd6c28b4
dlaurent <dlaurent@student.42.fr>
2018-11-01 12:01:35 +0100

    [fix] heredoc

--- src/terminal/read/read.c
+++ src/terminal/read/read.c
@@ -6,11 +6,28 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/01 16:10:01 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/17 23:48:55 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/01 11:59:20 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+static void	sh_close_all(t_shell *sh)
+{
+	t_heredocs	*tmp;
+
+	if (!(tmp = sh->heredocs))
+		return ;
+	while (tmp->closed)
+		tmp = tmp->next;
+	if (sh->buffer.ushift + ft_strlens(tmp->keyword) >= ARG_MAX)
+		return ;
+	ft_strcpy(sh->buffer.content + sh->buffer.ushift, tmp->keyword);
+	sh->buffer.unicode_len = ft_strlens(tmp->keyword);
+	sh->buffer.display_len = ft_strlenu(tmp->keyword);
+	ft_bzero(sh->read->line, LINE_SIZE);
+	sh->read->line[0] = '\n';
+}
 
 void		sh_read(t_shell *sh)
 {
@@ -22,8 +39,12 @@ void		sh_read(t_shell *sh)
 		if (sh->read->line[0] == 4 && !sh->modes.multiline
 		&& sh->buffer.display_len + sh->buffer.dshift)
 			sh_delete_current_char(sh);
-		else if (sh->read->line[0] == 4 && !sh->modes.multiline)
+		else if (sh->read->line[0] == 4
+		&& !sh->modes.multiline && !sh->modes.heredoc)
 			break ;
+		else if (sh->read->line[0] == 4
+		&& sh->modes.heredoc && !sh->buffer.display_len)
+			sh_close_all(sh);
 		sh_read_dispatcher(sh);
 		ft_bzero(sh->read->line, LINE_SIZE);
 	}
