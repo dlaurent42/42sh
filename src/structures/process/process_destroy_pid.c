106dc5f1a8f41d8946814b50dfbec3b9a29cf10a
Dav HOJT <davhojt@gmail.com>
2018-11-04 15:49:37 +0100

    [feat] renamed delete -> destroy

--- src/structures/process/process_destroy_pid.c
+++ src/structures/process/process_destroy_pid.c
@@ -1,18 +1,18 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   process_delete_pid.c                               :+:      :+:    :+:   */
+/*   process_destroy_pid.c                              :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/04 15:44:42 by dhojt             #+#    #+#             */
-/*   Updated: 2018/11/04 15:45:09 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/11/04 15:49:17 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-bool				process_delete_pid(t_shell *sh, pid_t pid)
+bool				process_destroy_pid(t_shell *sh, pid_t pid)
 {
 	return (process_destroy(sh, process_get_pid(sh, pid));
 }
