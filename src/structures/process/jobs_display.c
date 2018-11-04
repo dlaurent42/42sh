526f77d747dffa0abd72912ebb460bd1ea36f6f4
Dav HOJT <davhojt@gmail.com>
2018-11-04 17:48:10 +0100

    [feat] added header and 'no jobs' message.

--- src/structures/process/jobs_display.c
+++ src/structures/process/jobs_display.c
@@ -6,7 +6,7 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/04 16:30:14 by dhojt             #+#    #+#             */
-/*   Updated: 2018/11/04 17:14:31 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/11/04 17:29:37 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,9 +17,15 @@ void				jobs_display(t_shell *sh)
 	t_process		*process;
 
 	process = sh->process;
-	while (process)
+	if (!process)
+		ft_printf("jobs: no jobs\n");
+	else
 	{
-		process_display(process);
-		process = process->next;
+		ft_printf("jobs:\n");
+		while (process)
+		{
+			process_display(process);
+			process = process->next;
+		}
 	}
 }
