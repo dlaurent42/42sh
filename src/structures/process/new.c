9ed1f4efb9c9d1c78ed98d7b31ac794710bca3d8
Dav HOJT <davhojt@gmail.com>
2018-11-04 14:24:42 +0100

    [feat] Refactored new.

--- src/structures/process/new.c
+++ src/structures/process/new.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/04 13:24:37 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/04 13:29:44 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/04 14:24:11 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,16 +14,10 @@
 
 t_process			*process_new(void)
 {
-	t_process		*ret;
+	t_process		*new_process;
 
-	if (!(ret = (t_process *)ft_memalloc(sizeof(t_process))))
+	if (!(new_process = (t_process *)ft_memalloc(sizeof(t_process))))
 		return (NULL);
-	ret->id = 0;
-	ft_bzero((int)fd, sizeof(int) * 10);
-	ret->status = 0;
-	ret->pid = -1;
-	ret->cmd = NULL;
-	ret->next = NULL;
-	ret->head = NULL;
-	return (ret)
+	new_process->pid = -1;
+	return (new_process)
 }
