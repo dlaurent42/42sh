b32348f4915f368e5ea6ffd3cb563549863f59c8
adomas <adomas.zal@gmail.com>
2018-11-05 20:18:06 +0100

    [super messy]First execution running on jobs.

--- src/functions/jobs/background.c
+++ src/functions/jobs/background.c
@@ -1,26 +1,20 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   new.c                                              :+:      :+:    :+:   */
+/*   background.c                                       :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2018/11/05 14:01:39 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/05 19:39:59 by azaliaus         ###   ########.fr       */
+/*   Created: 2018/11/05 19:07:42 by azaliaus          #+#    #+#             */
+/*   Updated: 2018/11/05 19:10:19 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-t_job		*job_new(void)
+void		put_job_in_background(t_job *job, int cont)
 {
-	t_job		*job;
-
-	if (!(job = (t_job *)ft_memalloc(sizeof(t_job))))
-		return (NULL);
-	job->fd[0] = STDIN_FILENO;
-	job->fd[1] = STDOUT_FILENO;
-	job->fd[2] = STDERR_FILENO;
-	job->next = NULL;
-	return (job);
+	if (cont)
+		if (kill(-job->pgid, SIGCONT) < 0)
+			ft_putendl_fd("kill (SIGCONT)", 2);
 }
