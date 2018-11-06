0f548f9fb19c1ed700f93f47a494104791a7d576
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-06 10:06:39 +0100

    Signal for jobs.

--- src/terminal/signals/catch.c
+++ src/terminal/signals/catch.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/01 18:53:24 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/02 18:29:18 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/06 10:06:17 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,6 +25,13 @@ static void	sig_handler(int signo)
 		sh_sigint_reset(g_sh, "1");
 }
 
+void		sig_job(int signo)
+{
+	(void)signo;
+	ft_printf("Got signal from CTRL + Z\n");
+	ft_putendl_fd("Got signal from CTRL + Z", 2);
+}
+
 void		signal_catching(void)
 {
 	signal(SIGSEGV, sig_handler);
@@ -32,4 +39,5 @@ void		signal_catching(void)
 		ft_putendl_fd("\ncan't catch SIGWINCH", 2);
 	if (signal(SIGINT, sig_handler) == SIG_ERR)
 		ft_putendl_fd("\ncan't catch SIGINT", 2);
+	(g_sh->job) ? signal(SIGTSTP, sig_job) : signal(SIGTSTP, SIG_DFL);
 }
