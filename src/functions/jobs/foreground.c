f6043ab27413e5037d316813eab404bebb014c55
adomas <adomas.zal@gmail.com>
2018-11-06 22:28:15 +0100

    Foreground changes.

--- src/functions/jobs/foreground.c
+++ src/functions/jobs/foreground.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 17:06:51 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/05 18:13:56 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/06 20:06:47 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,12 @@ void		put_job_in_foreground(t_job *job, int cont)
 	tcsetpgrp(STDIN_FILENO, job->pgid);
 	if (cont)
 	{
-		tcsetattr(STDIN_FILENO, TCSADRAIN, &(g_sh->termios));
+		tcsetattr(STDIN_FILENO, TCSADRAIN, &(job->tmodes));
 		if (kill(-job->pgid, SIGCONT) < 0)
 			ft_putendl_fd("kill (SIGCONT)", 2);
 	}
 	job_wait(job);
-	tcsetpgrp(STDIN_FILENO, g_sh->pid);
-	tcgetattr(STDIN_FILENO, &(g_sh->termios));
+	tcsetpgrp(STDIN_FILENO, g_sh->shell_pgid);
+	tcgetattr(STDIN_FILENO, &(job->tmodes));
 	tcsetattr(STDIN_FILENO, TCSADRAIN, &(g_sh->termios));
 }
