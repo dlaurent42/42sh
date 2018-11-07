9106fd2a3271ec84581f3aa49e78ba953a03da87
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-07 15:23:59 +0100

    Signal default reset implamented.

--- include/prototypes.h
+++ include/prototypes.h
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/13 18:03:23 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/09 13:51:57 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/09 13:52:48 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -722,6 +722,7 @@ int						sh_get_start_rel_from_abs(t_shell *sh);
 void					signal_catching(void);
 void					sh_sigint_reset(t_shell *sh, char *last_return);
 void					sh_window_resize(t_shell *sh);
+void					signal_default(void);
 
 /*
 ** structures - ast
---
+++ src/terminal/signals/default.c
@@ -0,0 +1,23 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   default.c                                          :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2018/11/07 15:20:22 by azaliaus          #+#    #+#             */
+/*   Updated: 2018/11/07 15:20:46 by azaliaus         ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include "shell.h"
+
+void	signal_default(void)
+{
+	signal(SIGINT, SIG_DFL);
+	signal(SIGQUIT, SIG_DFL);
+	signal(SIGTSTP, SIG_DFL);
+	signal(SIGTTIN, SIG_DFL);
+	signal(SIGTTOU, SIG_DFL);
+	signal(SIGCHLD, SIG_DFL);
+}
