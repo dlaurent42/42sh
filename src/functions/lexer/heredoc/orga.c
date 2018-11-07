e20c1fbdc9a2fe1cb39950b009ebc63ab4ae4702
dlaurent <dlaurent@student.42.fr>
2018-11-07 11:04:17 +0100

    [norm]

--- src/functions/lexer/heredoc/orga.c
+++ src/functions/lexer/heredoc/orga.c
@@ -6,18 +6,40 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/04 13:39:09 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/04 14:39:35 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/07 11:02:16 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static void	sh_heredoc_repatriate(t_lexer *lexer, int i, int cmd_pos)
+static void	sh_heredoc_repatriate_loop(t_lexer *lexer, int i, int cmd_pos)
 {
 	int				j;
 	int				tmp_blank;
 	char			*tmp_id;
 	t_token_type	tmp_type;
+
+	j = i;
+	tmp_id = ft_strdups(lexer->tokens[i].id);
+	tmp_type = lexer->tokens[i].type;
+	tmp_blank = lexer->tokens[i].blank_before;
+	while (cmd_pos + 1 <= j)
+	{
+		ft_strdel(&(lexer->tokens[j].id));
+		lexer->tokens[j].id = ft_strdups(lexer->tokens[j - 1].id);
+		lexer->tokens[j].type = lexer->tokens[j - 1].type;
+		lexer->tokens[j].blank_before = lexer->tokens[j - 1].blank_before;
+		j--;
+	}
+	ft_strdel(&(lexer->tokens[j].id));
+	lexer->tokens[cmd_pos].id = tmp_id;
+	lexer->tokens[cmd_pos].type = tmp_type;
+	lexer->tokens[cmd_pos].blank_before = tmp_blank;
+}
+
+static void	sh_heredoc_repatriate(t_lexer *lexer, int i, int cmd_pos)
+{
+
 
 	i += 2;
 	if (i >= (int)lexer->size)
@@ -27,22 +49,7 @@ static void	sh_heredoc_repatriate(t_lexer *lexer, int i, int cmd_pos)
 	|| lexer->tokens[i].type == TOKEN_BACKQUOTE
 	|| lexer->tokens[i].type == TOKEN_WORD)
 	{
-		j = i;
-		tmp_id = ft_strdups(lexer->tokens[i].id);
-		tmp_type = lexer->tokens[i].type;
-		tmp_blank = lexer->tokens[i].blank_before;
-		while (cmd_pos + 1 <= j)
-		{
-			ft_strdel(&(lexer->tokens[j].id));
-			lexer->tokens[j].id = ft_strdups(lexer->tokens[j - 1].id);
-			lexer->tokens[j].type = lexer->tokens[j - 1].type;
-			lexer->tokens[j].blank_before = lexer->tokens[j - 1].blank_before;
-			j--;
-		}
-		ft_strdel(&(lexer->tokens[j].id));
-		lexer->tokens[cmd_pos].id = tmp_id;
-		lexer->tokens[cmd_pos].type = tmp_type;
-		lexer->tokens[cmd_pos].blank_before = tmp_blank;
+		sh_heredoc_repatriate_loop(lexer, i, cmd_pos);
 		cmd_pos++;
 		i++;
 	}
