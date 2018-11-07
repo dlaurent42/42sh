0a6139fe855fe80792df125d1aa04c22889aa77d
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-07 14:29:02 +0100

    Status updates fixed and normed.

--- src/functions/process/status.c
+++ src/functions/process/status.c
@@ -6,55 +6,58 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 17:15:58 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/07 10:25:17 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/07 14:28:34 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 #include <errno.h>
 
-int		process_status(pid_t pid, int status)
+/*
+**	When errno == ECHILD - there is no process to get.
+*/
+
+static int	process_status_err(pid_t pid)
+{
+	if (pid == 0 || errno == ECHILD)
+		return (-1);
+	ft_putendl_fd("waitpid error", 2);
+	return (-1);
+}
+
+static int	process_found(t_process *p, int status)
+{
+	p->status = status;
+	if (WIFSTOPPED(status))
+		p->stopped = 1;
+	else
+	{
+		p->completed = 1;
+		if (WIFSIGNALED(status))
+			ft_putendl_fd("Terminated by singal!", 2);
+	}
+	return (0);
+}
+
+int			process_status(pid_t pid, int status)
 {
 	t_job		*job;
 	t_process	*p;
 
-	if (pid > 0)
+	if (pid <= 0)
+		return (process_status_err(pid));
+	job = g_sh->job;
+	while (job)
 	{
-		job = g_sh->job;
-		while (job)
+		p = job->first_process;
+		while (p)
 		{
-			p = job->first_process;
-			while (p)
-			{
-				if (p->pid == pid)
-				{
-					p->status = status;
-					if (WIFSTOPPED(status))
-						p->stopped = 1;
-					else
-					{
-						p->completed = 1;
-						if (WIFSIGNALED(status))
-							ft_putendl_fd("Terminated by singal!", 2);
-					}
-					return (0);
-				}
-				p = p->next;
-			}
-			job = job->next;
+			if (p->pid == pid)
+				return (process_found(p, status));
+			p = p->next;
 		}
-		fprintf(stderr, "No child process %d.\n", pid);
-		return (-1);
+		job = job->next;
 	}
-	else if (pid == 0 || errno == ECHILD) /* No processes ready to report.  */
-	{
-		ft_putendl_fd("[process_status]No process is ready", 2);
-		return (-1);
-	}
-	else
-	{
-    	/* Other weird errors.  */
-		ft_putendl_fd("waitpid error", 2);
-		return (-1);
-	}
+	ft_putendl_fd("NO child process", 2);
+	return (-1);
 }
