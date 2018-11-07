45cc5ce2fc7d72aaefcd0091d0c72be2472c9a05
dlaurent <dlaurent@student.42.fr>
2018-11-07 11:05:33 +0100

    [norm]

--- src/functions/lexer/heredoc/orga.c
+++ src/functions/lexer/heredoc/orga.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/04 13:39:09 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/07 11:02:16 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/07 11:05:24 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -39,8 +39,6 @@ static void	sh_heredoc_repatriate_loop(t_lexer *lexer, int i, int cmd_pos)
 
 static void	sh_heredoc_repatriate(t_lexer *lexer, int i, int cmd_pos)
 {
-
-
 	i += 2;
 	if (i >= (int)lexer->size)
 		return ;
@@ -77,7 +75,7 @@ char		sh_heredoc_orga(t_lexer *lexer, char status)
 		|| lexer->tokens[i].type == TOKEN_WORD)
 			command_position++;
 		else if (lexer->tokens[i].type == TOKEN_HEREDOC)
-			sh_heredoc_repatriate(lexer, i, command_position);	
+			sh_heredoc_repatriate(lexer, i, command_position);
 		i++;
 	}
 	return (status);
