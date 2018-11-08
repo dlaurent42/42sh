3e20025d258ac79a35ba02e01b27a4da35631ea5
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-08 19:16:12 +0100

    Job COMMAND name fixed.

--- src/functions/jobs/execute.c
+++ src/functions/jobs/execute.c
@@ -6,11 +6,53 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/08 10:52:11 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/08 15:00:33 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/08 19:14:23 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+static int	str2_sizes(char	**argv)
+{
+	int		i;
+	int		ret;
+
+	ret = 0;
+	i = 0;
+	while (argv[i])
+	{
+		ret += ft_strlen(argv[i]);
+		if (argv[i + 1])
+			ret++;
+		i++;
+	}
+	return (ret);
+}
+
+static char	*ft_str2tostr(char **argv)
+{
+	int		i;
+	int		x;
+	int		cursor;
+	char	*ret;
+
+	if (!(ret = ft_strnew(str2_sizes(argv))))
+		return (NULL);
+	i = 0;
+	cursor = 0;
+	while (argv[i])
+	{
+		x = 0;
+		while (argv[i][x])
+		{
+			ret[cursor++] = argv[i][x];
+			x++;
+		}
+		(argv[i + 1]) ? ret[cursor++] = ' ' : (0);
+		i++;
+	}
+	return (ret);
+}
 
 int			sh_do_job(t_shell *sh, char **argv, t_env *env)
 {
@@ -21,6 +63,7 @@ int			sh_do_job(t_shell *sh, char **argv, t_env *env)
 	ret = STATUS_OK;
 	job = job_new();
 	job->notifications = sh->jc_muted;
+	job->command = ft_str2tostr(argv);
 	job_add(sh, job);
 	p = process_new();
 	p->argv = argv;
