bd476e8b9a89ff040bfc57b98879b30ffdd9dff3
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-08 11:50:39 +0100

    Changed message for signal (SIGCONT).

--- src/functions/jobs/foreground.c
+++ src/functions/jobs/foreground.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/05 17:06:51 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/08 11:38:59 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/08 11:50:25 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@ void		put_job_in_foreground(t_job *job, int cont)
 	{
 		tcsetattr(STDIN_FILENO, TCSADRAIN, &(job->tmodes));
 		if (kill(-job->pgid, SIGCONT) < 0)
-			ft_putendl_fd("kill (SIGCONT)", 2);
+			ft_putendl_fd("sh: process was killed by signal (SIGCONT)", 2);
 	}
 	job_wait(job);
 	tcsetpgrp(STDIN_FILENO, g_sh->shell_pgid);
