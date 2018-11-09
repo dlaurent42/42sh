7b96a2e646334c199553c53d33f924ddd806eb68
Dav HOJT <davhojt@gmail.com>
2018-11-09 12:13:13 +0100

    Added macro for FB and BG

--- include/prototypes.h
+++ include/prototypes.h
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/13 18:03:23 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/09 13:58:36 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/09 13:59:02 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -148,6 +148,7 @@ char					sh_history_option_warn(
 */
 char					buildin_jobs_bg(t_shell *sh, char **argv);
 char					buildin_jobs_fg(t_shell *sh, char **argv);
+char					buildin_jobs_launch(t_shell *sh, char **argv, int mode);
 char					buildin_jobs(t_shell *sh, char **argv);
 
 /*
