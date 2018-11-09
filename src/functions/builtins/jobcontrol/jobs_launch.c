7ff207bdcc0566af9e39bfd136ba9e8634231eff
Dav HOJT <davhojt@gmail.com>
2018-11-09 14:13:12 +0100

    created get process by pid.

--- src/functions/builtins/jobcontrol/jobs_launch.c
+++ src/functions/builtins/jobcontrol/jobs_launch.c
@@ -6,17 +6,44 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 12:10:04 by dhojt             #+#    #+#             */
-/*   Updated: 2018/11/09 13:16:01 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/11/09 14:12:17 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-t_job				*get_latest_job(t_shell *sh)
+t_process			*get_process_by_pid(t_job *job, pid_t pid)
 {
-	t_job			*job;
+	t_process		*process;
 
-	job = sh->job;
+	process = NULL;
+	while (job)
+	{
+		proces = job->first_process;
+		while (process)
+		{
+			if (process->pid = pid)
+			   return (process);	
+			process = process->next;
+		}
+		job = job->next;
+	}
+	return (process);
+}
+
+t_job				*get_job_by_id(t_job *job, int id)
+{
+	while (job)
+	{
+		if (job->id = id)
+			break ;
+		job = job->next;
+	}
+	return (job);
+}
+
+t_job				*get_latest_job(t_job *job)
+{
 	while (jobs && job->next)
 		job = job->next;
 	return (job);
@@ -28,11 +55,16 @@ char				buildin_jobs_launch(t_shell *sh, char **argv, int mode)
 
 	job = sh->job;
 	if (!ft_count_argv((void **)argv))
-		job = get_latest_job(sh);
+		job = get_latest_job(job);
+	else if (!ft_count_argv((void **)argv) == 2)
+	{
+		if (!ft_strcmps(argv[0], "-i"))
+			job = job_by_id(job, ft_atoi(argv[1]));
+	}
 	else
 		job = sh->jobs;
 	if (!job)
-		ft_putendl("bg: no current job");
+		ft_printf("%s: no jobs\n", (mode == JOBS_FG) ? "fg" : "bg");
 	else
 	{
 		if (!ft_count_argv((void **)argv))
@@ -54,5 +86,4 @@ char				buildin_jobs_launch(t_shell *sh, char **argv, int mode)
 		}
 	}
 	return (STATUS_OK);
-	// return (jobs_finder(sh, argv, JOBS_BG));
 }
