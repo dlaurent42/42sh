868656ed8cb98394a6e74d6ac20051b632cb8c1c
Dav HOJT <davhojt@gmail.com>
2018-11-09 14:53:14 +0100

    finished fg and bg

--- src/functions/builtins/jobcontrol/jobs_launch.c
+++ src/functions/builtins/jobcontrol/jobs_launch.c
@@ -6,11 +6,26 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 12:10:04 by dhojt             #+#    #+#             */
-/*   Updated: 2018/11/09 14:27:31 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/11/09 14:50:34 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
+
+bool				is_valid_number(char *str)
+{
+	int				len;
+
+	len = 0;
+	while (*str)
+	{
+		if (!ft_isdigit(*str) || len > 10)
+			return (false);
+		len++;
+		str++;
+	}
+	return (true);
+}
 
 t_job				*get_process_by_pid(t_job *job, pid_t pid)
 {
@@ -19,23 +34,23 @@ t_job				*get_process_by_pid(t_job *job, pid_t pid)
 	process = NULL;
 	while (job)
 	{
-		proces = job->first_process;
+		process = job->first_process;
 		while (process)
 		{
-			if (process->pid = pid)
-			   return (process);	
+			if (process->pid == pid)
+				return (job);	
 			process = process->next;
 		}
 		job = job->next;
 	}
-	return (jobs);
+	return (job);
 }
 
 t_job				*get_job_by_id(t_job *job, int id)
 {
 	while (job)
 	{
-		if (job->id = id)
+		if (job->id == id)
 			break ;
 		job = job->next;
 	}
@@ -44,7 +59,7 @@ t_job				*get_job_by_id(t_job *job, int id)
 
 t_job				*get_latest_job(t_job *job)
 {
-	while (jobs && job->next)
+	while (job && job->next)
 		job = job->next;
 	return (job);
 }
@@ -53,18 +68,16 @@ char				buildin_jobs_launch(t_shell *sh, char **argv, int mode)
 {
 	t_job		*job;
 
-	job = sh->job;
-	if (!ft_count_argv((void **)argv))
-		job = get_latest_job(job);
-	else if (!ft_count_argv((void **)argv) == 2)
+	job = NULL;
+	if (ft_count_argv((void **)argv) == 0)
+		job = get_latest_job(sh->job);
+	else if (ft_count_argv((void **)argv) == 2 && is_valid_number(argv[1]))
 	{
 		if (!ft_strcmps(argv[0], "-i"))
-			job = get_job_by_id(job, ft_atoi(argv[1]));
+			job = get_job_by_id(sh->job, ft_atoi(argv[1]));
 		else if (!ft_strcmps(argv[0], "-p"))
-			job = get_process_by_pid(job, ft_atoi(argv[1]));
+			job = get_process_by_pid(sh->job, ft_atoi(argv[1]));
 	}
-	else
-		job = sh->jobs;
 	if (!job)
 		ft_printf("%s: no jobs\n", (mode == JOBS_FG) ? "fg" : "bg");
 	else
