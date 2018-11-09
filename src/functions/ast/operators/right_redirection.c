23a8845b07736333c22223f9897203f09c41ba93
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-09 17:22:36 +0100

    Fixed right redirection.

--- src/functions/ast/operators/right_redirection.c
+++ src/functions/ast/operators/right_redirection.c
@@ -6,7 +6,7 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/25 12:46:49 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/01 19:21:23 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/09 17:22:22 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -52,15 +52,13 @@ char		execute_right_redirection(t_shell *sh, t_token_tree *tree)
 
 	last = tree->right;
 	ops = O_WRONLY | O_CREAT;
-	ops |= ((!ft_strcmp(tree->tokens[0], ">")
-		|| !ft_strcmp(tree->tokens[0] + 1, ">")) ? O_TRUNC : O_APPEND);
+	ops |= ((!ft_strcmp(tree->tokens[0], ">>")) ? O_APPEND : O_TRUNC);
 	while (last)
 	{
 		if (last->type == 1)
 		{
 			ops = O_WRONLY | O_CREAT;
-			ops |= ((!ft_strcmp(tree->tokens[0], ">")
-				|| !ft_strcmp(tree->tokens[0] + 1, ">")) ? O_TRUNC : O_APPEND);
+			ops |= ((!ft_strcmp(tree->tokens[0], ">>")) ? O_APPEND : O_TRUNC);
 		}
 		file = (last->left ? last->left->tokens[0] : last->tokens[0]);
 		if ((fd = open(file, ops, 0644)) == -1)
