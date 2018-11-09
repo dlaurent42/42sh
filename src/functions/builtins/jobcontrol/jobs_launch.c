755f6a41ccd335b5b90263e68cd1e3230d67bdeb
Dav HOJT <davhojt@gmail.com>
2018-11-09 14:34:18 +0100

    modified proves to work with job.

--- src/functions/builtins/jobcontrol/jobs_launch.c
+++ src/functions/builtins/jobcontrol/jobs_launch.c
@@ -6,13 +6,13 @@
 /*   By: dhojt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 12:10:04 by dhojt             #+#    #+#             */
-/*   Updated: 2018/11/09 14:12:17 by dhojt            ###   ########.fr       */
+/*   Updated: 2018/11/09 14:27:31 by dhojt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-t_process			*get_process_by_pid(t_job *job, pid_t pid)
+t_job				*get_process_by_pid(t_job *job, pid_t pid)
 {
 	t_process		*process;
 
@@ -28,7 +28,7 @@ t_process			*get_process_by_pid(t_job *job, pid_t pid)
 		}
 		job = job->next;
 	}
-	return (process);
+	return (jobs);
 }
 
 t_job				*get_job_by_id(t_job *job, int id)
@@ -59,31 +59,15 @@ char				buildin_jobs_launch(t_shell *sh, char **argv, int mode)
 	else if (!ft_count_argv((void **)argv) == 2)
 	{
 		if (!ft_strcmps(argv[0], "-i"))
-			job = job_by_id(job, ft_atoi(argv[1]));
+			job = get_job_by_id(job, ft_atoi(argv[1]));
+		else if (!ft_strcmps(argv[0], "-p"))
+			job = get_process_by_pid(job, ft_atoi(argv[1]));
 	}
 	else
 		job = sh->jobs;
 	if (!job)
 		ft_printf("%s: no jobs\n", (mode == JOBS_FG) ? "fg" : "bg");
 	else
-	{
-		if (!ft_count_argv((void **)argv))
-		{
-			while (job->next)
-				job = job->next;
-			job_continue(job, mode); // <- 1 - foreground | 0 - background
-		}
-		else if (ft_count_argv((void **)argv) == 1)
-		{
-			// Dav: do stuff
-			// Return proper job and run this
-			job_continue(job, mode); // <- 1 - foreground | 0 - background
-		}
-		else
-		{
-			ft_putendl("fg: too much arguments");
-			return (STATUS_OK);
-		}
-	}
+		job_continue(job, mode);
 	return (STATUS_OK);
 }
