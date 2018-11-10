a39a5fd2ec4fae740a66580622348c1dccb60d5b
dlaurentn <dlaurent@student.42.fr>
2018-11-10 13:32:47 +0100

    [fix] window size 0

--- src/structures/shell/new.c
+++ src/structures/shell/new.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/08/25 18:31:18 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/08 13:34:24 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/10 13:32:09 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -33,6 +33,8 @@ t_shell		*sh_new(int argc, char **argv, char **environ)
 	if (sh_set_termios(sh) != STATUS_OK && argc == 1)
 		error_malloc_sh(sh);
 	sh_set_window(sh, argc);
+	if (sh->window.width == 0 || sh->window.height == 0)
+		error_malloc_sh(sh);
 	sh->jc = (argc > 1) ? 0 : 1;
 	sh->shell_pgid = getpid();
 	sh->env = env_new(sh, environ);
