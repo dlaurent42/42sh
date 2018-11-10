1549a27689119f9e6c09228888c467957748ddc9
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-10 16:13:14 +0100

    [fix] on \b backtick.

--- src/functions/backtick/execute.c
+++ src/functions/backtick/execute.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   execute.c                                          :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
+/*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/31 19:19:08 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/03 18:07:30 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/10 16:12:20 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -28,7 +28,7 @@ static char		*backtick_process(t_shell *sh, t_token_tree *list)
 	{
 		dup2(fd[1], 1);
 		close(fd[1]);
-		write(1, "\b", 1);
+		write(1, " ", 1);
 		(list) ? execute_tree(sh, list) : error_execution_tree();
 		exit(sh->exe);
 	}
@@ -44,6 +44,7 @@ static char		*backtick_process(t_shell *sh, t_token_tree *list)
 char			*execute_backtick(t_shell *sh, t_lexer lexer)
 {
 	char			*ret;
+	char			*tmp;
 	t_token_tree	*list;
 
 	ret = NULL;
@@ -51,6 +52,12 @@ char			*execute_backtick(t_shell *sh, t_lexer lexer)
 	reorganise_tokens(&list);
 	list = build_token_tree(list);
 	ret = backtick_process(sh, list);
+	if (ret)
+	{
+		tmp = ret;
+		ret = ft_strdups(ret + 1);
+		ft_strdel(&tmp);
+	}
 	clean_tree(list);
 	return (ret);
 }
