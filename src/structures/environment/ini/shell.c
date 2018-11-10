b6a0ed4bda7d322ee57147fb1b8bace97eb1c3a1
dlaurentn <dlaurent@student.42.fr>
2018-11-10 12:01:34 +0100

    [feature] edit SHELL env var

--- src/structures/environment/ini/shell.c
+++ src/structures/environment/ini/shell.c
@@ -6,7 +6,7 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/11/09 21:05:17 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/11/10 10:49:56 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/10 12:01:24 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,17 +14,11 @@
 
 void		sh_env_init_shell(t_shell *sh, char *path)
 {
-	char	*curr_path;
 	char	*new_name;
 
-	new_name = NULL;
-	curr_path = NULL;
-	curr_path = getcwd(curr_path, PATH_MAX);
+	new_name = realpath(path, NULL);
 	if (!env_search(sh->env, "SHELL") && sh->env->count + 1 >= sh->env->size)
 		return ;
-	new_name = ft_strjoins(curr_path, path + 1);
-	new_name = sh_cd_parse_path(new_name);
 	env_insert(sh, sh->env, "SHELL", new_name);
 	ft_strdel(&new_name);
-	ft_strdel(&curr_path);
 }
