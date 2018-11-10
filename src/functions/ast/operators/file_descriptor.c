b8e9e2758818782236d8339bc51ded995d5e5ca1
Adomas Žaliauskas <adomas.zal@gmail.com>
2018-11-10 16:41:21 +0100

    [fix] better vesion for `<&`.

--- src/functions/ast/operators/file_descriptor.c
+++ src/functions/ast/operators/file_descriptor.c
@@ -6,13 +6,13 @@
 /*   By: azaliaus <azaliaus@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/10/26 10:34:55 by azaliaus          #+#    #+#             */
-/*   Updated: 2018/11/09 21:12:41 by azaliaus         ###   ########.fr       */
+/*   Updated: 2018/11/10 16:40:37 by azaliaus         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static int	check_fd(int fd)
+static int		check_fd(int fd)
 {
 	int		dummy;
 	int		ret;
@@ -26,15 +26,39 @@ static int	check_fd(int fd)
 	return (ret);
 }
 
-static char	do_aggregation(t_shell *sh, t_token_tree *tree, char *front,
+static char		do_redir(
+					t_shell *sh, t_token_tree *tree, char *front, char *cmd)
+{
+	int			fd[2];
+	int			stdin;
+	char		ret;
+	char		*back;
+
+	if (!(back = get_back_descriptor(cmd)))
+		error_malloc_reader(sh, "token");
+	fd[0] = ft_atoi(front);
+	fd[1] = ft_atoi(back);
+	if (fd[1] > 9)
+		return (error_file_descriptor(&back));
+	(fd[1] < 0) ? fd[1] = 0 : (0);
+	stdin = dup(fd[1]);
+	dup2(fd[0], fd[1]);
+	ret = execute_tree(sh, tree->left);
+	dup2(stdin, fd[1]);
+	(stdin != -1) ? close(stdin) : (0);
+	ft_strdel(&back);
+	return (ret);
+}
+
+static char		do_aggregation(t_shell *sh, t_token_tree *tree, char *front,
 								char *cmd)
 {
-	int		closed;
 	char	*back;
 	char	ret;
 	int		fd[3];
 
-	closed = (!ft_strncmp(cmd, ">&", 2) ? 1 : 0);
+	if (!(!ft_strncmp(cmd, ">&", 2)))
+		return (do_redir(sh, tree, front, cmd));
 	if (!(back = get_back_descriptor(cmd)))
 		error_malloc_reader(sh, "token");
 	fd[0] = ft_atoi(front);
@@ -52,7 +76,7 @@ static char	do_aggregation(t_shell *sh, t_token_tree *tree, char *front,
 	return (ret);
 }
 
-char		execute_fd_aggr(t_shell *sh, t_token_tree *tree)
+char			execute_fd_aggr(t_shell *sh, t_token_tree *tree)
 {
 	size_t		len;
 	char		ret;
