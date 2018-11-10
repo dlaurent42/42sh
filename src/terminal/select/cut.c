3a2107c841e92d37f4991311904696cab0214bf3
dlaurentn <dlaurent@student.42.fr>
2018-11-10 02:16:09 +0100

    [fix] cut (now slower but safer)

--- src/terminal/select/cut.c
+++ src/terminal/select/cut.c
@@ -6,59 +6,26 @@
 /*   By: dlaurent <dlaurent@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2018/09/25 19:43:32 by dlaurent          #+#    #+#             */
-/*   Updated: 2018/10/04 19:15:46 by dlaurent         ###   ########.fr       */
+/*   Updated: 2018/11/10 02:15:41 by dlaurent         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "shell.h"
 
-static void	sh_cut_delete(t_shell *sh, int start, int len)
-{
-	while (sh->buffer.content[start + len])
-	{
-		sh->buffer.content[start] = sh->buffer.content[start + len];
-		start++;
-	}
-	while (sh->buffer.content[start])
-	{
-		sh->buffer.content[start] = '\0';
-		start++;
-	}
-	sh->buffer.display_len = ft_strlenu(sh->buffer.content + sh->buffer.ushift);
-	sh->buffer.unicode_len = ft_strlens(sh->buffer.content + sh->buffer.ushift);
-}
-
-static void	sh_move_cursor(t_shell *sh, int x, int y)
-{
-	sh->cursor.y = (sh->buffer.display_len + sh->prompt.len_mod)
-		/ sh->window.width;
-	sh->cursor.x = (sh->cursor.y)
-		? (sh->buffer.display_len + sh->prompt.len_mod) % sh->window.width
-		: sh->buffer.display_len;
-	sh->cursor.abs_pos = sh->buffer.display_len;
-	sh->cursor.rel_pos = sh->buffer.unicode_len;
-	sh_move_to_xy(sh, x, y);
-}
-
 void		sh_cut_selection(t_shell *sh)
 {
-	int	x;
-	int	y;
-	int	len;
 	int	start;
+	int	stop;
 
-	x = sh->cursor.x;
-	y = sh->cursor.y;
 	if (!sh->modes.select)
 		return ;
 	sh->modes.browse = FALSE;
-	len = sh_get_selection_len(sh);
-	start = sh->selection.start_rel;
+	stop = sh->selection.stop;
+	start = sh->selection.start_abs;
 	sh_copy_selection(sh);
-	sh_cut_delete(sh, sh->buffer.ushift + start, len);
-	sh_move_home(sh);
-	ft_putstr(K_DEL_ALL);
-	ft_putstr(sh->buffer.content + sh->buffer.ushift);
-	sh_move_cursor(sh, x, y);
+	while (sh->cursor.abs_pos < stop)
+		sh_move_right(sh);
+	while (sh->cursor.abs_pos > start)
+		sh_delete_char(sh);
 	sh->modes.select = FALSE;
 }
